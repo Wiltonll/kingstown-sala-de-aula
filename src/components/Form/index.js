e4afa26266f80bf2ac7f7f7a5115773161e81ad8
JoseHenriiquep <josehenriquenetinho@gmail.com>
2024-05-04 23:57:54 -0300

    .

---
+++ src/components/Form/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from './Form.module.css'
+
+
+function Form(){
+    return(
+        <div className={styles.body}>
+                    <form className="center-align">
+                        <div className="row">
+                            <div className="input-field col s6" id={styles.input}>
+                                <input id="email" type="email" className="validate"/>
+                                <label for="email">Email</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input id="password" type="password" className="validate"/>
+                                <label for="password">Password</label>
+                            </div>
+                        </div>
+                    </form>
+                
+        </div>
+    );
+}
+
+export default Form;
