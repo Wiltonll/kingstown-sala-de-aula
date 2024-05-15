f289110b7dd52f016fc8217446ee8af815b13869
JoseHenriiquep <josehenriquenetinho@gmail.com>
2024-05-15 09:34:39 -0300

    .

---
+++ src/components/Logo/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './Logo.module.css';
+
+function Logo(){
+    return(
+        <div className={styles.logotipo}>
+            
+        </div>
+    );
+}
