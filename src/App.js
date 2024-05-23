7b3a717b5537fb96214b7eed70d521fb998043a8
JoseHenriiquep <josehenriquenetinho@gmail.com>
2024-05-22 22:31:26 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-
 import Home from './pages/Home';
 import About from './pages/About';
 import Settings from './pages/Settings';
 import React, { useEffect } from "react";
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import Form from './components/Form';
-import Turma from './components/Turma'
-
 
 function App() {
   return (
     <>
-    <Form />
-      {/*
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Home />}></Route>
@@ -20,7 +15,6 @@ function App() {
           <Route path="/settings" exact element={<Settings />}></Route>
         </Routes>
       </BrowserRouter>
-      */}
     </>
   );
 }
