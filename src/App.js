68dd3ac0eb1a0c3123803c2d250907b9f73b27c2
Joao Paulo <joaopaulosouza540@gmail.com>
2024-08-28 23:21:53 -0300

    #

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import About from './pages/About';
 import Settings from './pages/Settings';
 import Sidenav from './components/Sidenav'; // Importe o componente de barra lateral aqui
 import CriarTurmaPage from './components/CriarTurma'; // Importe a página de criação de turma aqui
+import Form from './components/Form';
 
 function App() {
   return (
     <>
-      <Router>
-        <Sidenav /> {/* Renderize a barra lateral em torno das rotas */}
+      <Router> 
+      {/*
+        <Sidenav /> 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/settings" element={<Settings />} />
-          <Route path="/criar-turma" element={<CriarTurmaPage />} /> {/* Rota para a página de criação de turma */}
-        </Routes>
+          <Route path="/criar-turma" element={<CriarTurmaPage />} /> 
+        </Routes> 
+      */}   
+      <Form/>
       </Router>
     </>
   );
