c0c9a43c9c8115f160163b113c094e3aa773743b
Joao Paulo <joaopaulosouza540@gmail.com>
2024-09-03 22:11:14 -0300

    .

--- Front-End/src/components/Form/index.js
+++ Front-End/src/components/Form/index.js
@@ -1,10 +1,28 @@
-import React from "react";
-import styles from './Form.module.css';
-import Imagem from '../../img/logo_bg_branco.png';
-
-
+import styles from './Form.module.css'
+import Imagem from '../../img/logo_bg_branco.png'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function Form(){
+    const [showPassword, setShowPassword] = React.useState(false);
+  
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+    const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+    };
+  
+    const handleMouseUpPassword = (event) => {
+      event.preventDefault();
+    };
+
     return(
         <div className={styles.body}>
             <div className={styles.containerImg}>
@@ -13,7 +31,29 @@ function Form(){
                 </div>
             </div>
             <form>
-                
+                <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                    <div>
+                        <FormControl sx={{ m: 1, width: '200px'}} variant="standard">
+                            <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                            <Input
+                            id="standard-adornment-password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    onMouseUp={handleMouseUpPassword}
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                                </InputAdornment>
+                            }
+                            />
+                        </FormControl>
+                    </div>
+                </Box>
             </form>
         </div>
     );
