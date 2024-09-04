668cd521e13dbb8f2c45b11de58aef520173bd8d
Joao Paulo <joaopaulosouza540@gmail.com>
2024-09-03 22:29:04 -0300

    .

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -5,20 +5,12 @@ import About from './pages/About';
 import Settings from './pages/Settings';
 import Sidenav from './components/Sidenav'; // Importe o componente de barra lateral aqui
 import CriarTurmaPage from './components/CriarTurma'; // Importe a página de criação de turma aqui
-import Form from './components/Form';
+import Form from '../src/components/Form';
 
 function App() {
   return (
     <>
-      <Router>
-        <Sidenav /> {/* Renderize a barra lateral em torno das rotas */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/criar-turma" element={<CriarTurmaPage />} /> {/* Rota para a página de criação de turma */}
-        </Routes>
-      </Router>
+      <Form/>
     </>
   );
 }
--- Front-End/src/components/Form/index.js
+++ Front-End/src/components/Form/index.js
@@ -1,22 +1,59 @@
-import React from "react";
-import styles from './Form.module.css';
-
+import styles from './Form.module.css'
+import Imagem from '../../img/logo_bg_branco.png'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function Form(){
+    const [showPassword, setShowPassword] = React.useState(false);
+  
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+    const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+    };
+  
+    const handleMouseUpPassword = (event) => {
+      event.preventDefault();
+    };
+
     return(
         <div className={styles.body}>
             <div className={styles.containerImg}>
                 <div id={styles.img}>
+                    <img src={Imagem} alt="Logo"/>
                 </div>
             </div>
             <form>
-                <h4>Faça login com sua conta Google </h4>
-                <div className={styles.container_loginGoogle}>
-                    <button className={styles.btnGoogle}>
-                        <div className={styles.logo_google}></div>
-                        Login with Google
-                    </button>
-                </div>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                    <div>
+                        <FormControl sx={{ m: 1, width: '200px'}} variant="standard">
+                            <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                            <Input
+                            id="standard-adornment-password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    onMouseUp={handleMouseUpPassword}
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                                </InputAdornment>
+                            }
+                            />
+                        </FormControl>
+                    </div>
+                </Box>
             </form>
         </div>
     );
