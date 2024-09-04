108e1cb2356f05c2b841cc12d09f23964b9d2e02
Joao Paulo <joaopaulosouza540@gmail.com>
2024-09-04 11:16:07 -0300

    .

--- Front-End/src/components/Form2/index.js
+++ Front-End/src/components/Form2/index.js
@@ -2,18 +2,33 @@ import "./Form.module.css"
 import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LockIcon from '@mui/icons-material/Lock';
+import styles from './Form.module.css'
+
+import { useState } from "react";
 
 const Form2 = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        alert("Enviando os dados:" + username + " - " + password)
+
+        console.log("Teste",username, password)
+        console.log("Envio")
+    }
+
   return (
     <div className="container">
-        <form>
-             <h1>Acesse o sistema:</h1>
+        <form onSubmit = {handleSubmit}>
+             <h1>Faça seu login</h1>
              <div> 
-                <input type="text" placeholder= "Matricula"/>
+                <input type="text" placeholder= "Matricula" onChange={(e) => setUsername(e.target.value)}/>
                 <AccountCircleIcon className= "icon" />
              </div>
              <div> 
-                <input type="password" placeholder= "Senha"/>
+                <input type="password" placeholder= "Senha" onChange={(e) => setPassword(e.target.value)}/>
                 <LockIcon className="icon" />
             </div>
             <a href="#"> Esqueci a senha</a>
