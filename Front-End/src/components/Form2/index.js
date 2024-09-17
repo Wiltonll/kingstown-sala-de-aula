2e054e08cece4912b3a76c08be0e08fe3c0fc0d3
Victor Moura <victorhugomoura16@gmail.com>
2024-09-17 20:06:44 -0300

    Atualizações no css

--- Front-End/src/components/Form2/index.js
+++ Front-End/src/components/Form2/index.js
@@ -7,36 +7,36 @@ import Image from '../../img/logo_branco_png.png';
 import { useState } from "react";
 
 const Form2 = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
+  const handleSubmit = (event) => {
+    event.preventDefault()
 
-        alert("Enviando os dados:" + username + " - " + password)
+    alert("Enviando os dados:" + username + " - " + password)
 
-        console.log("Teste",username, password)
-        console.log("Envio")
-    }
+    console.log("Teste", username, password)
+    console.log("Envio")
+  }
 
   return (
     <div className={styles.body}>
       <div className={styles.container}>
-        <img src={Image} alt=""/>
-        <form onSubmit = {handleSubmit}>
-             <h1>Faça seu login</h1>
-             <div className={styles.inputfield}> 
-                <input type="text" placeholder= "Login" onChange={(e) => setUsername(e.target.value)}/>
-                <AccountCircleIcon className={styles.icon}/>
-             </div>
-             <div className={styles.inputfield}>  
-                <input type="password" placeholder= "Senha" onChange={(e) => setPassword(e.target.value)} />
-                <LockIcon className={styles.icon} />
-            </div>
-            <a href="#" className={styles.link}> Esqueci a senha</a>
-            <button>Entrar</button>
-        </form>    
-      </div>       
+        <img src={Image} alt="Logo da empresa" />
+        <form onSubmit={handleSubmit}>
+          <h1>Faça seu login</h1>
+          <div className={styles.inputfield}>
+            <input type="text" placeholder="Login" onChange={(e) => setUsername(e.target.value)} />
+            <AccountCircleIcon className={styles.icon} />
+          </div>
+          <div className={styles.inputfield}>
+            <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} />
+            <LockIcon className={styles.icon} />
+          </div>
+          <a href="#" className={styles.link}> Esqueci a senha</a>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   )
 }
