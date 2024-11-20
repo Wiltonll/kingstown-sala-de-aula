ed6d4a790591da725b1fb76e2926dc756c7e039a
João Paulo <joaopaulosouza540@gmail.com>
2024-11-20 16:32:54 -0300

    NovasTelasCriarTurmasECriarAlunos

--- Front-End/src/components/ProfessorDashboard/CriarAlunos.js
+++ Front-End/src/components/ProfessorDashboard/CriarAlunos.js
@@ -1,38 +1,165 @@
-import React from 'react';
-import { TextField, Button, Box, Typography } from '@mui/material';
-import styles from './styles/CriarAlunos.module.css';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Box, Paper } from '@mui/material';
 
 const CriarAlunos = () => {
-    return (
-        <Box className={styles.container}>
-            <Typography className={styles.title} variant="h4" component="h1" gutterBottom>
-                Criar Alunos
-            </Typography>
-            <Box component="form" noValidate autoComplete="off">
-                <TextField
-                    className={styles.formField}
-                    label="Nome do Aluno"
-                    variant="outlined"
-                    fullWidth
-                />
-                <TextField
-                    className={styles.formField}
-                    label="Email do Aluno"
-                    variant="outlined"
-                    fullWidth
-                />
-                <TextField
-                    className={styles.formField}
-                    label="Idade do Aluno"
-                    variant="outlined"
-                    fullWidth
-                />
-                <Button className={styles.createButton} variant="contained" color="primary">
-                    Criar Aluno
-                </Button>
-            </Box>
-        </Box>
-    );
+  const [nomeAluno, setNomeAluno] = useState('');
+  const [emailAluno, setEmailAluno] = useState('');
+  const [senhaAluno, setSenhaAluno] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Aluno Criado com sucesso:', { nomeAluno, emailAluno, senhaAluno });
+  };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        backgroundColor: '#FFFFF', 
+      }}
+    >
+      <Paper
+        elevation={4}
+        sx={{
+          padding: 4,
+          maxWidth: 400,
+          width: '100%',
+          borderRadius: 2,
+          backgroundColor: '#FFFF', 
+        }}
+      >
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{
+            color: '#5922A0', // Roxo escuro
+            fontWeight: 'bold',
+            textAlign: 'center',
+          }}
+        >
+          Criar Aluno
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Nome do Aluno(a)"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={nomeAluno}
+            onChange={(e) => setNomeAluno(e.target.value)}
+            required
+            sx={{
+              // Define a cor do rótulo
+              '& .MuiInputLabel-root': {
+                color: '#5922A0',
+              },
+              // Define a cor do rótulo quando focado
+              '& .MuiInputLabel-root.Mui-focused': {
+                color: '#5922A0',
+              },
+              // Configura o contorno do campo
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#5922A0', // Borda inicial
+                },
+                '&:hover fieldset': {
+                  borderColor: '#7E57C2', // Borda ao passar o mouse
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#5922A0', // Borda quando focado
+                },
+              },
+            }}
+          />
+
+
+          <TextField
+            label="Email do Aluno"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            type="number"
+            value={emailAluno}
+            onChange={(e) => setEmailAluno(e.target.value)}
+            required
+            sx={{
+              // Define a cor do rótulo
+              '& .MuiInputLabel-root': {
+                color: '#5922A0',
+              },
+              // Define a cor do rótulo quando focado
+              '& .MuiInputLabel-root.Mui-focused': {
+                color: '#5922A0',
+              },
+              // Configura o contorno do campo
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#5922A0', // Borda inicial
+                },
+                '&:hover fieldset': {
+                  borderColor: '#7E57C2', // Borda ao passar o mouse
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#5922A0', // Borda quando focado
+                },
+              },
+            }}
+          />
+          <TextField
+            label="Senha para o email"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={senhaAluno}
+            onChange={(e) => setSenhaAluno(e.target.value)}
+            required
+            sx={{
+              // Define a cor do rótulo
+              '& .MuiInputLabel-root': {
+                color: '#5922A0',
+              },
+              // Define a cor do rótulo quando focado
+              '& .MuiInputLabel-root.Mui-focused': {
+                color: '#5922A0',
+              },
+              // Configura o contorno do campo
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#5922A0', // Borda inicial
+                },
+                '&:hover fieldset': {
+                  borderColor: '#7E57C2', // Borda ao passar o mouse
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#5922A0', // Borda quando focado
+                },
+              },
+            }}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            fullWidth
+            sx={{
+              marginTop: 2,
+              paddingY: 1.5,
+              backgroundColor: '#5922A0',
+              color: '#FFD105',
+              fontWeight: 'bold',
+              '&:hover': {
+                backgroundColor: '#7E57C2',
+              },
+            }}
+          >
+            Criar Aluno
+          </Button>
+        </form>
+      </Paper>
+    </Box>
+  );
 };
 
 export default CriarAlunos;
--- Front-End/src/components/ProfessorDashboard/CriarSalas.js
+++ Front-End/src/components/ProfessorDashboard/CriarSalas.js
@@ -1,55 +1,163 @@
 import React, { useState } from 'react';
-import { TextField, Button, Typography, Box } from '@mui/material';
-import styles from './styles/CriarSalas.module.css';
+import { TextField, Button, Typography, Box, Paper } from '@mui/material';
 
 const CriarSalas = () => {
   const [nomeSala, setNomeSala] = useState('');
-  const [capacidade, setCapacidade] = useState('');
+  const [descricao, setDescricao] = useState('');
   const [horario, setHorario] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Sala Criada:', { nomeSala, capacidade, horario });
+    console.log('Sala Criada:', { nomeSala, descricao, horario });
   };
 
   return (
-    <Box className={styles.container}>
-      <Typography className={styles.title} variant="h4" gutterBottom>
-        Criar Sala
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          className={styles.formField}
-          label="Nome da Sala"
-          variant="outlined"
-          fullWidth
-          value={nomeSala}
-          onChange={(e) => setNomeSala(e.target.value)}
-          required
-        />
-        <TextField
-          className={styles.formField}
-          label="Capacidade"
-          variant="outlined"
-          fullWidth
-          type="number"
-          value={capacidade}
-          onChange={(e) => setCapacidade(e.target.value)}
-          required
-        />
-        <TextField
-          className={styles.formField}
-          label="Dias/Horários"
-          variant="outlined"
-          fullWidth
-          value={horario}
-          onChange={(e) => setHorario(e.target.value)}
-          required
-        />
-        <Button className={styles.createButton} variant="contained" color="primary" type="submit">
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        backgroundColor: '#FFFFF', 
+      }}
+    >
+      <Paper
+        elevation={4}
+        sx={{
+          padding: 4,
+          maxWidth: 400,
+          width: '100%',
+          borderRadius: 2,
+          backgroundColor: '#FFFF', 
+        }}
+      >
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{
+            color: '#5922A0', // Roxo escuro
+            fontWeight: 'bold',
+            textAlign: 'center',
+          }}
+        >
           Criar Sala
-        </Button>
-      </form>
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Nome da Sala"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={nomeSala}
+            onChange={(e) => setNomeSala(e.target.value)}
+            required
+            sx={{
+              // Define a cor do rótulo
+              '& .MuiInputLabel-root': {
+                color: '#5922A0',
+              },
+              // Define a cor do rótulo quando focado
+              '& .MuiInputLabel-root.Mui-focused': {
+                color: '#5922A0',
+              },
+              // Configura o contorno do campo
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#5922A0', // Borda inicial
+                },
+                '&:hover fieldset': {
+                  borderColor: '#7E57C2', // Borda ao passar o mouse
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#5922A0', // Borda quando focado
+                },
+              },
+            }}
+          />
+
+
+          <TextField
+            label="Descrição"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            type="number"
+            value={descricao}
+            onChange={(e) => setDescricao(e.target.value)}
+            required
+            sx={{
+              // Define a cor do rótulo
+              '& .MuiInputLabel-root': {
+                color: '#5922A0',
+              },
+              // Define a cor do rótulo quando focado
+              '& .MuiInputLabel-root.Mui-focused': {
+                color: '#5922A0',
+              },
+              // Configura o contorno do campo
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#5922A0', // Borda inicial
+                },
+                '&:hover fieldset': {
+                  borderColor: '#7E57C2', // Borda ao passar o mouse
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#5922A0', // Borda quando focado
+                },
+              },
+            }}
+          />
+          <TextField
+            label="Dias/Horários"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={horario}
+            onChange={(e) => setHorario(e.target.value)}
+            required
+            sx={{
+              // Define a cor do rótulo
+              '& .MuiInputLabel-root': {
+                color: '#5922A0',
+              },
+              // Define a cor do rótulo quando focado
+              '& .MuiInputLabel-root.Mui-focused': {
+                color: '#5922A0',
+              },
+              // Configura o contorno do campo
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#5922A0', // Borda inicial
+                },
+                '&:hover fieldset': {
+                  borderColor: '#7E57C2', // Borda ao passar o mouse
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#5922A0', // Borda quando focado
+                },
+              },
+            }}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            fullWidth
+            sx={{
+              marginTop: 2,
+              paddingY: 1.5,
+              backgroundColor: '#5922A0',
+              color: '#FFD105',
+              fontWeight: 'bold',
+              '&:hover': {
+                backgroundColor: '#7E57C2',
+              },
+            }}
+          >
+            Criar Sala
+          </Button>
+        </form>
+      </Paper>
     </Box>
   );
 };
